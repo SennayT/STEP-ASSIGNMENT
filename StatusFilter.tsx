0e02b8772c5e5a8b356d89d051c7eac7981fbcd6
Samuel Haileamriam <samuelhailemariam4@gmail.com>
2022-02-07 16:39:41 +0300

    A function that renders the filtered result was defiend

--- StatusFilter.tsx
+++ StatusFilter.tsx
@@ -17,3 +17,28 @@ const StatusFilter = ({ value: status, onChange }: StatusFilterProps) => {
       );
   }
 
+ const renderedFilters = Object.keys(StatusFilters).map((key) => {
+    // const value = StatusFilters[key];
+    const value = getValue(key);
+
+    const handleClick = () => onChange(value);
+    const className = value === "status" ? "selected" : "";
+
+    return (
+      <li key={value}>
+        <button className={className} onClick={handleClick}>
+          {key}
+        </button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="filters statusFilters">
+      <h5>Filter by status</h5>
+      <ul>{renderedFilters}</ul>
+    </div>
+  );
+};
+
+export default StatusFilter;
