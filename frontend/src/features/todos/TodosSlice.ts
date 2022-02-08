93d144ade34c8f3c0dda99ff6c55d7ff189bf1e7
Samuel Haileamriam <samuelhailemariam4@gmail.com>
2022-02-08 11:45:49 +0300

    todos inital state was defiend and a function that fetch todo items was defiend

---
+++ frontend/src/features/todos/TodosSlice.ts
@@ -0,0 +1,35 @@
+import {
+  createAsyncThunk,
+  createEntityAdapter,
+  createSelector,
+  createSlice,
+  PayloadAction,
+} from "@reduxjs/toolkit";
+import { stat } from "fs";
+import TodoService, { TodoLocal, TodoRemote } from "../../service/TodoService";
+import { RootState } from "../../store";
+import { StatusFilters } from "../filters/filterSlice";
+
+export interface Todo {
+  id: number;
+  completed: boolean;
+  text: string;
+  color?: string;
+}
+
+type TodoEntity = { [id: number]: Todo };
+
+const todosAdapter = createEntityAdapter<Todo>({
+  selectId: (todo) => todo.id,
+});
+const initialState = todosAdapter.getInitialState({
+  status: "idle",
+});
+
+const todoService: TodoService = new TodoRemote();
+
+export const fetchTodos = createAsyncThunk("todos/fetchTodos", async () => {
+  const todos = await todoService.getAllTodos();
+  console.log(todos);
+  return todos;
+});
