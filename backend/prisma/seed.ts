32182638a5741112842b3b34ae976da4879e5a20
Sennay <notsenay@gmail.com>
2022-02-09 03:41:51 +0300

    add more items to seed

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -28,6 +28,18 @@ const todos: Prisma.TodoCreateInput[] = [
     completed: true,
     color: "green",
   },
+
+  {
+    text: "buy shoes",
+    completed: true,
+    color: "red",
+  },
+
+  {
+    text: "finish internsip",
+    completed: true,
+    color: "green",
+  },
 ];
 
 async function main() {
