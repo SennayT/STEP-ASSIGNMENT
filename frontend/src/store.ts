f13d15d4be4427891b18023dc09f181a048e2f07
Samuel Hailemariam <samuelhailemariam4@gmail.com>
2022-02-09 06:49:06 -0800

    store.ts file was created

---
+++ frontend/src/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import todosReducer from "./features/todos/TodosSlice";
+import filterReducer from "./features/filters/filterSlice";
+const store = configureStore({
+  reducer: {
+    todos: todosReducer,
+    filters: filterReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store
