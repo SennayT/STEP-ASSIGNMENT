d628b8988197ab81912f264124baea53ce9be4ce
Robel Shewangzaw <61440928+Robel-shewan@users.noreply.github.com>
2022-02-09 06:15:08 +0300

    adding the TodoService in Backend fie

---
+++ backend/service/todoService.ts
@@ -0,0 +1,88 @@
+import { Prisma, Todo, PrismaClient } from "@prisma/client";
+
+type TodoInput = Prisma.TodoCreateInput;
+
+export interface TodoService {
+  getAllTodos(): Promise<Todo[]>;
+  getTodo(id: number): Promise<Todo>;
+  addTodo(todo: TodoInput): Promise<Todo>;
+  addTodoFromText(text: string): Promise<Todo>;
+  updateTodo(id: number, newInfo: TodoInput): Promise<Todo>;
+  deleteTodo(id: number): Promise<void>;
+}
+
+export class TodoServiceImpl implements TodoService {
+  private prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+  updateTodo(id: number, newInfo: Prisma.TodoCreateInput): Promise<Todo> {
+    return new Promise(async (resolve, reject) => {
+      const todo = await this.prisma.todo.update({
+        where: {
+          id,
+        },
+        data: {
+          ...newInfo,
+        },
+      });
+      resolve(todo);
+    });
+  }
+  deleteTodo(id: number): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+      await this.prisma.todo.delete({
+        where: {
+          id,
+        },
+      });
+      resolve();
+    });
+  }
+  getTodo(id: number): Promise<Todo> {
+    return new Promise<Todo>(async (resolve, reject) => {
+      const todo = await this.prisma.todo.findUnique({
+        where: {
+          id,
+        },
+      });
+      if (todo === null) {
+        reject(`Todo with id ${id} not found`);
+      } else {
+        resolve(todo);
+      }
+    });
+  }
+
+  addTodo(todo: Prisma.TodoCreateInput): Promise<Todo> {
+    return new Promise(async (resolve, reject) => {
+      const newTodo = await this.prisma.todo.create({
+        data: {
+          completed: todo.completed,
+          color: todo.color,
+          text: todo.text,
+        },
+      });
+      resolve(newTodo);
+    });
+  }
+
+  addTodoFromText(text: string): Promise<Todo> {
+    return new Promise(async (resolve, reject) => {
+      const todo = await this.prisma.todo.create({
+        data: {
+          text,
+        },
+      });
+      resolve(todo);
+    });
+  }
+
+  getAllTodos(): Promise<Todo[]> {
+    return new Promise<Todo[]>(async (resolve, reject) => {
+      const res = await this.prisma.todo.findMany();
+      resolve(res);
+    });
+  }
+}
